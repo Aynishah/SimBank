b4f0a0a9ac2e64ca58dfde879101fcbe7babd13b
Aynishah <aynishah10@gmail.com>
2026-01-16 14:15:46 +0600

    added sout and visa payment class

--- src/kg/sim_bank/payment/QRPayment.java
+++ src/kg/sim_bank/payment/QRPayment.java
@@ -3,6 +3,7 @@ package kg.sim_bank.payment;
 public class QRPayment {
     public static void main(String[] args) {
         System.out.println("QR Payment");
+        System.out.println("Hello World");
     }
 
 }
