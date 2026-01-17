ca91e23d72ad2626d18ee397d92c8fc1f44c24b0
Aynishah <aynishah10@gmail.com>
2026-01-17 17:11:46 +0600

    Resolved conflict

---
+++ src/kg/sim_bank/delivery/Delivery.java
@@ -0,0 +1,10 @@
+package kg.sim_bank.delivery;
+
+public class Delivery {
+    public static void main(String[] args) {
+        System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+        System.out.println("Delivery Kover-Samolet");
+    }
+
+}
