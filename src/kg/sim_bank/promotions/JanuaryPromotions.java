79aece3f309414308e85f5cb91cd82e139f1e973
Aynishah <aynishah10@gmail.com>
2026-01-18 21:32:09 +0600

    January Promo Aynishah.
    January Promotion1
    
    January promotion2
    
    January promorion3
    
    January promorion4
    
    January promorion5
    
    January promorion6

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,16 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+        System.out.println("January Promotion");
+        System.out.println("January Promotion123");
+        System.out.println("January Promotion1234");
+        System.out.println("January Promotion1235");
+        System.out.println("January Promotion12356");
+        System.out.println("January Promotion123567");
+        System.out.println("January Promotion1235678");
+        System.out.println("January Promotion12356789");
+
+    }
+
+}
