fa2e3023c0b4dbd0829cd34443ce4945057e6588
Aynishah <aynishah10@gmail.com>
2026-01-19 09:45:07 +0600

    Revert "my name"
    This reverts commit fb3bee0bd1b6bff10b3e8159d6b73150bfdf80ad.

--- src/revertAndReset/RevertDemo1.java
+++ src/revertAndReset/RevertDemo1.java
@@ -3,7 +3,6 @@ package revertAndReset;
 public class RevertDemo1 {
     public static void main(String[] args) {
         System.out.println("first one");
-        System.out.println(My name is Aynishah);
     }
 
 }
