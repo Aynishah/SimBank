ce156241acdff6f94493a820320f34331e727568
Aynishah <aynishah10@gmail.com>
2026-01-19 10:30:32 +0600

    otp123

--- src/revertAndReset/Reset.java
+++ src/revertAndReset/Reset.java
@@ -3,6 +3,7 @@ package revertAndReset;
 public class Reset {
     public static void main(String[] args) {
         System.out.println("Reset one");
+        System.out.println("otp123");
     }
 
 }
